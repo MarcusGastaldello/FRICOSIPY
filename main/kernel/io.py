fb4eeb7791c63f6f18bb4a11b6b97f4867c93c38
Marcus Gastaldello <marcus.a.gastaldello@gmail.com>
2025-12-16 16:44:23 +0100

    Add files via upload

--- main/kernel/io.py
+++ main/kernel/io.py
@@ -207,7 +207,7 @@ class IOClass:
         self.RESULT.attrs['Thermal_conductivity_method'] = thermal_conductivity_method
         self.RESULT.attrs['Specific_heat_method'] = specific_heat_method
         self.RESULT.attrs['Water_percolation_method'] = preferential_percolation_method
-        self.RESULT.attrs['Surface_temperature_method'] = surface_temperature_method
+        self.RESULT.attrs['Surface_temperature_solver'] = surface_temperature_solver
         self.RESULT.attrs['Snow_metamorphism_method'] = snow_metamorphism_method
 
         # Subsurface Remeshing Options:    
