274b59a72cbf874940717fe5f08ee08354a4aaa0
Marcus Gastaldello <marcus.a.gastaldello@gmail.com>
2026-02-10 10:45:58 +0100

    Add files via upload

--- main/kernel/init.py
+++ main/kernel/init.py
@@ -12,6 +12,7 @@
 """
 
 import numpy as np
+import math as mt
 from constants import *
 from config import *
 from main.kernel.grid import *
@@ -20,7 +21,7 @@ from main.kernel.grid import *
 # Initialise Snowpack
 # =================== #
 
-def init_snowpack():
+def init_snowpack(STATIC):
     """ This function initialises the snowpack / glacier for the first simulation time step """
 	
     # Initialise layer variable arrays
@@ -33,78 +34,143 @@ def init_snowpack():
     layer_hydro_year = []
     layer_grain_size = []
 
+    # Override initial glacier & snow heights if glacier thickness provided in the static file
+    if 'THICKNESS' in list(STATIC.keys()):
+        snowheight = min(STATIC.THICKNESS.values, initial_snowheight)
+        glacier_height = max(STATIC.THICKNESS.values - initial_snowheight, 0)
+    else:
+        snowheight = initial_snowheight
+        glacier_height = initial_glacier_height
+
+    # Base elevation:
+    base_elevation = STATIC.ELEVATION.values - (snowheight + glacier_height)
+
     # =================== #
     # Snowpack & Glacier:
     # =================== #    
 
     # Snow / firn layers     
-    if (initial_snowheight > 0.0):
-        nlayers = int(initial_snowheight / initial_snow_layer_heights)
-        dT = (initial_upper_temperature - initial_lower_temperature) / (initial_snowheight + initial_glacier_height)
-        if nlayers == 0:
-            layer_heights = np.array([initial_snowheight])
-            layer_densities = np.array([initial_upper_snowpack_density])
-            layer_T = np.array([initial_upper_temperature])
-            layer_liquid_water = np.array([0.0])
-            layer_refreeze = np.array([0.0])
-            layer_firn_refreeze = np.array([0.0])
-            layer_hydro_year = np.array([0.0])
-            layer_grain_size = np.array([initial_snow_grain_size])
-        elif nlayers > 0:
-            drho = (initial_upper_snowpack_density - initial_lower_snowpack_density) / initial_snowheight
-            layer_heights = np.ones(nlayers) * (initial_snowheight / nlayers)
-            layer_densities = np.array([initial_upper_snowpack_density - drho * (initial_snowheight / nlayers) * i for i in range(1, nlayers + 1)])
-            layer_T = np.array([initial_upper_temperature - dT * (initial_snowheight / nlayers) * i for i in range(1, nlayers + 1)])
-            layer_liquid_water = np.zeros(nlayers)
-            layer_refreeze = np.zeros(nlayers)
-            layer_firn_refreeze = np.zeros(nlayers)
-            layer_hydro_year = np.zeros(nlayers)
-            layer_grain_size = initial_snow_grain_size + (layer_densities / snow_ice_threshold) * (initial_ice_grain_size - initial_snow_grain_size)
+    if (snowheight > 0.0):
 
-        # Glacier layers
-        nlayers = int(initial_glacier_height / initial_glacier_layer_heights)
-        layer_heights = np.append(layer_heights, np.ones(nlayers) * initial_glacier_layer_heights)
-        layer_densities = np.append(layer_densities, np.ones(nlayers)*ice_density)
-        layer_T = np.append(layer_T, [layer_T[-1] - dT * initial_glacier_layer_heights * i for i in range(1, nlayers + 1)])
-        layer_liquid_water = np.append(layer_liquid_water, np.zeros(nlayers))
-        layer_refreeze = np.append(layer_refreeze, np.zeros(nlayers))
-        layer_firn_refreeze = np.append(layer_firn_refreeze, np.zeros(nlayers))
-        layer_hydro_year = np.append(layer_hydro_year, np.zeros(nlayers))
-        layer_grain_size = np.append(layer_grain_size, np.full(nlayers, initial_ice_grain_size))
+        if snowheight < coarse_layer_threshold:
+
+            # Calculate the number of full snow layers:
+            n_full_layers = mt.floor(snowheight / initial_snow_layer_heights)
+
+            # Surface layer height
+            surface_layer_height = snowheight % initial_snow_layer_heights
+            if surface_layer_height < minimum_snow_layer_height:
+                layer_heights = np.ones(n_full_layers) * initial_snow_layer_heights
+                layer_heights[0] = layer_heights[0] + surface_layer_height
+            else:
+                layer_heights = np.concatenate((np.array([surface_layer_height]), (np.ones(n_full_layers) * initial_snow_layer_heights)))
+        
+            # Number of snow layers
+            n_snow_layers = len(layer_heights)
+
+        else:
+
+            # Calculate the number of full fine snow layers: 
+            n_full_fine_layers = mt.floor(coarse_layer_threshold / initial_snow_layer_heights)
+            fine_layer_heights = np.ones(n_full_fine_layers) * initial_snow_layer_heights
+
+            # Calculate the number of full coarse snow layers:
+            n_full_coarse_layers = mt.floor((snowheight - coarse_layer_threshold) / maximum_coarse_layer_height)
+            coarse_layer_heights = np.ones(n_full_coarse_layers) * maximum_coarse_layer_height
+
+            # Surface layer height
+            surface_layer_height = snowheight - (np.sum(fine_layer_heights) + np.sum(coarse_layer_heights))
+            if surface_layer_height < minimum_snow_layer_height:
+                layer_heights = np.concatenate((fine_layer_heights, coarse_layer_heights))
+                layer_heights[0] = layer_heights[0] + surface_layer_height
+            else:
+                layer_heights = np.concatenate((np.array([surface_layer_height]), fine_layer_heights, coarse_layer_heights))
+
+            # Number of snow layers
+            n_snow_layers = len(layer_heights)
+
+        # Calculate midpoint depth of layers
+        midpoint_depths = np.cumsum(layer_heights) - (layer_heights / 2.0)
+        
+        # Calculate temperature and density gradients
+        dT = ((initial_upper_temperature + zero_temperature) - (initial_lower_temperature + zero_temperature)) / (snowheight + glacier_height)
+        drho = (initial_upper_snowpack_density - initial_lower_snowpack_density) / snowheight
+
+        # Initialise snow layer variables
+        layer_densities = initial_upper_snowpack_density - (drho * midpoint_depths)
+        layer_T = (initial_upper_temperature + zero_temperature) - (dT * midpoint_depths)
+        layer_liquid_water = np.zeros(n_snow_layers)
+        layer_refreeze = np.zeros(n_snow_layers)
+        layer_firn_refreeze = np.zeros(n_snow_layers)
+        layer_hydro_year = np.zeros(n_snow_layers)
+        layer_grain_size = initial_snow_grain_size + (layer_densities / snow_ice_threshold) * (initial_ice_grain_size - initial_snow_grain_size)
+
+        # Number of glacier layers
+        n_glacier_layers = int(glacier_height / initial_glacier_layer_heights)
+
+        # Glacier layer heights:
+        glacier_layer_heights = np.ones(n_glacier_layers) * initial_glacier_layer_heights
+
+        # Calculate midpoint depth of layers
+        midpoint_depths = (np.cumsum(glacier_layer_heights) - (glacier_layer_heights / 2.0)) + np.sum(layer_heights)
+
+        # Initialise full glacier layer variables
+        layer_T = np.concatenate((layer_T, (initial_upper_temperature + zero_temperature) - (dT * midpoint_depths)))
+        layer_heights = np.concatenate((layer_heights, np.ones(n_glacier_layers) * initial_glacier_layer_heights))
+        layer_densities = np.concatenate((layer_densities, np.ones(n_glacier_layers) * ice_density))
+        layer_liquid_water = np.concatenate((layer_liquid_water, np.zeros(n_glacier_layers)))
+        layer_refreeze = np.concatenate((layer_refreeze, np.zeros(n_glacier_layers)))
+        layer_firn_refreeze = np.concatenate((layer_firn_refreeze, np.zeros(n_glacier_layers)))
+        layer_hydro_year = np.concatenate((layer_hydro_year, np.zeros(n_glacier_layers)))
+        layer_grain_size = np.concatenate((layer_grain_size, np.full(n_glacier_layers, initial_ice_grain_size)))
     
     # ============= #
     # Glacier only:
     # ============= #
 
     else:
-        nlayers = int(initial_glacier_height / initial_glacier_layer_heights)
-        dT = (initial_upper_temperature - initial_lower_temperature) / initial_glacier_height
-        layer_heights = np.ones(nlayers) * initial_glacier_layer_heights
-        layer_densities = np.ones(nlayers) * ice_density
-        layer_T = np.array([initial_upper_temperature - dT * initial_glacier_layer_heights * i for i in range(1, nlayers + 1)])
-        layer_liquid_water = np.zeros(nlayers)
-        layer_refreeze = np.zeros(nlayers)
-        layer_firn_refreeze = np.zeros(nlayers)
-        layer_hydro_year = np.zeros(nlayers)
-        layer_grain_size = np.full(nlayers, initial_ice_grain_size)
+
+        # Number of glacier layers
+        n_glacier_layers = int(glacier_height / initial_glacier_layer_heights)
+
+        # Glacier layer heights:
+        glacier_layer_heights = np.ones(n_glacier_layers) * initial_glacier_layer_heights        
+
+        # Calculate midpoint depth of layers
+        midpoint_depths = np.cumsum(glacier_layer_heights) - (glacier_layer_heights / 2.0)
+
+        # Calculate temperature gradient
+        dT = ((initial_upper_temperature + zero_temperature) - (initial_lower_temperature + zero_temperature)) / glacier_height
+
+        # Initialise glacier layer variables
+        layer_heights = np.ones(n_glacier_layers) * initial_glacier_layer_heights
+        layer_densities = np.ones(n_glacier_layers) * ice_density
+        layer_T = (initial_upper_temperature + zero_temperature) - (dT * midpoint_depths)
+        layer_liquid_water = np.zeros(n_glacier_layers)
+        layer_refreeze = np.zeros(n_glacier_layers)
+        layer_firn_refreeze = np.zeros(n_glacier_layers)
+        layer_hydro_year = np.zeros(n_glacier_layers)
+        layer_grain_size = np.full(n_glacier_layers, initial_ice_grain_size)
 
     # ================ #
     # Initialise GRID:
     # ================ #
 
-    GRID = Grid(np.array(layer_heights, dtype = np.float64), 
-                np.array(layer_densities, dtype = np.float64), 
-                np.array(layer_T, dtype = np.float64), 
-                np.array(layer_liquid_water, dtype = np.float64), 
-                np.array(layer_refreeze, dtype = np.float64), 
-                np.array(layer_firn_refreeze, dtype = np.float64), 
-                np.array(layer_hydro_year, dtype = 'int32'), 
-                np.array(layer_grain_size, dtype = np.float64), 
-                None, 
-                None, 
-                None, 
-                None, 
-                None)
+    GRID = Grid(layer_heights.astype(np.float64), 
+                layer_densities.astype(np.float64), 
+                layer_T.astype(np.float64),
+                layer_T.astype(np.float64).copy(), 
+                layer_liquid_water.astype(np.float64), 
+                layer_refreeze.astype(np.float64), 
+                layer_firn_refreeze.astype(np.float64), 
+                layer_hydro_year.astype(np.int32), 
+                layer_grain_size.astype(np.float64),
+                float(base_elevation),
+                None,   # layer_ice_fraction
+                None,   # new_snow_height
+                None,   # new_snow_timestamp
+                None,   # old_snow_timestamp
+                None)   # fresh_snow_timestamp
     
     return GRID
 
